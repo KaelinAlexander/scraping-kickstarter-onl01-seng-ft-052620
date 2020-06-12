5b0fe039e5f72b699a9f3367c2f7569b1d40d8be
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-12 19:12:42 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,10 +1,37 @@
 require "nokogiri"
 require "pry"
+<<<<<<< HEAD
+=======
+
+  # projects: kickstarter.css("li.project.grid_4")
+  # title: project.css("h2.bbcard_name strong a").text
+  # image: project.css("div.project-thumbnail a img").attribute("src").value
+  # description: project.css("p.bbcard_blurb").text
+  # location: project.css("ul.project-meta span.location-name").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+>>>>>>> ba9cf1fbfa4127e5ee5c2987fa864905f001328f
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
+<<<<<<< HEAD
     binding.pry
+=======
+  
+  projects = {}
+  
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+    }
+  end
+  
+  projects
+>>>>>>> ba9cf1fbfa4127e5ee5c2987fa864905f001328f
 end
 
 create_project_hash
